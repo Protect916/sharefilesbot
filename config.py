aa48d9878874e492daf78b0d0238e975980cb58b
Protect916 <81868267+Protect916@users.noreply.github.com>
2021-04-03 18:25:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,1 @@
-token = '1700397844:AAFLQ5eS5F-HQOeV5B--4BH5_fj_O5H8tik'
-database_name = 'music.db'  # Файл с базой данных
-shelve_name = 'shelve.db'  # Файл с хранилищем
+token = 'укажите свой токен'
